443c8afbc78e64a1f5ac03b71a727267bb516be1
vsutinah <52406232+vsutinah@users.noreply.github.com>
2020-03-26 14:44:09 +1100

    Basic polling functionality completed

---
+++ django crash course/pollster/polls/views.py
@@ -0,0 +1,46 @@
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+from django.urls import reverse
+
+from .models import Question, Choice
+
+
+# 1st view: Get questions & display them
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+# 2nd view: Show specific question and choices
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist!")
+    return render(request, 'polls/detail.html', {'question': question })
+
+# 3rd view: Get question and display results
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question })
+
+# 4th view: Vote for question choice
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay question voting form
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. Prevents data from being posted twice if
+        # user hits Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
